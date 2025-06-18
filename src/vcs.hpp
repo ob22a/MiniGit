444f65a68dcf537e7f15ee4cc5fcb7cc0928c3d8
dagi1218 <dagmawiephrem44@gmail.com>
2025-06-18 20:48:37 +0300

    Update vcs.hpp

--- src/vcs.hpp
+++ src/vcs.hpp
@@ -1,1 +1,27 @@
 
+#ifndef VCS_HPP
+#define VCS_HPP
+
+#include <string>
+#include <vector>
+
+class VCS {
+public:
+    VCS(); // constructor
+
+    bool init();  // create .minigit structure
+    bool add(const std::string& filename);
+    bool commit(const std::string& message);
+    void log() const;
+    bool branch(const std::string& branchName);
+    bool checkout(const std::string& name); // can be branch or commit
+    bool merge(const std::string& branchName);
+
+private:
+    std::string getCurrentCommitHash() const;
+    bool writeBlob(const std::string& content, const std::string& hash);
+    bool updateIndex(const std::string& filename, const std::string& hash);
+    std::string computeHash(const std::string& filepath);
+};
+
+#endif
