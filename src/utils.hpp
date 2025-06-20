33f92c29a49af38df68f74449209d644d1be5bab
Aronabiyu <aaronabiyu07@gmail.com>
2025-06-20 22:54:02 +0300

    Update utils.hpp

--- src/utils.hpp
+++ src/utils.hpp
@@ -13,21 +13,7 @@ namespace utils {
 }
 
 #endif // UTILS_HPP
-#ifndef UTILS_HPP
-#define UTILS_HPP
 
-#include <string>
-#include <vector>
-
-namespace utils {
-
-    std::string trim(const std::string& str);
-    void displayError(const std::string& message);
-    void showDiff(const std::string& content1, const std::string& content2,
-                  const std::string& label1, const std::string& label2);
-}
-
-#endif // UTILS_HPP
 
 
 
